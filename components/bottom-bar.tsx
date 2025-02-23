f5f3c50c6f908ad33ff3b7c75d65011ccda5cc82
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2025-02-23 22:19:20 +0530

    convertuing bottom bar from floating to flex boxed at bottom.

--- components/bottom-bar.tsx
+++ components/bottom-bar.tsx
@@ -6,11 +6,10 @@ import { Button } from "./ui/button";
 export function BottomBar() {
   const router: string = usePathname();
   const activeButtonStyle: string =
-    "bg-primaryContainer text-onPrimaryContainer rounded-full transition ease-in duration-500";
-  const idleButtonStyle: string =
-    "bg-transparent text-primary rounded-full transition ease-out duration-100";
+    "bg-primaryContainer text-onPrimaryContainer rounded-full";
+  const idleButtonStyle: string = "bg-transparent text-primary rounded-full";
   return (
-    <footer className="fixed bottom-0 left-0 right-0 pt-2 pb-2 pl-1 pr-1 z-50 bg-background dark:bg-secondary rounded-t-xl drop-shadow-4xl">
+    <footer className="bg-background dark:bg-secondary py-1 drop-shadow-4xl">
       <div className="flex justify-around">
         <Button
           className={router === "/" ? activeButtonStyle : idleButtonStyle}
