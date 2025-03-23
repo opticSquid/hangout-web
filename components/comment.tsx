8502b3671d1dcdb0cfa1623e5b10ce43d329f8d1
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2025-03-23 14:04:34 +0530

    fixed commet component to display correct user name and picture

--- components/comment.tsx
+++ components/comment.tsx
@@ -1,27 +1,40 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { getTimeDifferenceFromUTC } from "@/lib/time-difference";
 import { CommentInterface } from "@/lib/types/comment-interface";
+import { ProfileData } from "@/lib/types/get-profile-response";
 import { Dot, MessageSquareReply } from "lucide-react";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { PhotoViewer } from "./photo-viewer";
 
-export function Comment({
+export async function Comment({
   comment,
   postId,
 }: {
   comment: CommentInterface;
   postId: string;
 }) {
+  const profileResponse: Response = await fetch(
+    `${process.env.NEXT_PUBLIC_PROFILE_API_URL}/profile/${comment.userId}`,
+    {
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+    }
+  );
+  const profileData: ProfileData = await profileResponse.json();
+
   return (
     <>
       <div className="m-1 flex flex-row items-start gap-2 pl-2 pr-2">
-        <Avatar className="w-12 h-12">
-          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-          <AvatarFallback delayMs={500}>CN</AvatarFallback>
-        </Avatar>
+        <PhotoViewer
+          filename={profileData.profilePicture.filename}
+          rounded={true}
+          radius="small"
+        />
         <div className="flex flex-col">
           <div className="flex flex-row items-center text-base text-neutral-400 dark:text-neutral-500 font-semibold tracking-tighter">
-            <div>{comment.userId}</div>
+            <div>{profileData.name}</div>
             <Dot size={16} />
             <div>{getTimeDifferenceFromUTC(comment.createdAt)}</div>
           </div>
