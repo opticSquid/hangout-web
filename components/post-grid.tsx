4eb5cc5bd4022e6498c9e550438bbdd56ebb380d
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2025-03-23 16:44:13 +0530

    plays only visible video in profile page

--- components/post-grid.tsx
+++ components/post-grid.tsx
@@ -2,15 +2,15 @@
 
 import { useSessionContext } from "@/lib/hooks/session-provider";
 import { ParticularPostInterface } from "@/lib/types/particular-post-interface";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Post } from "./post";
 import { EmptyFeed } from "./empty-feed";
 
 export function PostGrid() {
   const [sessionState] = useSessionContext();
-  const [postList, setPostList] = useState<
-    ParticularPostInterface[] | undefined
-  >(undefined);
+  const [postList, setPostList] = useState<ParticularPostInterface[]>([]);
+  const [visiblePostId, setVisiblePostId] = useState<string | null>(null);
+
   useEffect(() => {
     async function fetchPosts() {
       const response: Response = await fetch(
@@ -29,14 +29,40 @@ export function PostGrid() {
     }
     fetchPosts();
   }, [sessionState.accessToken]);
+
+  /**
+   * Calculates the 75% th element of the list. When that elment becomes visible it triggers additional data load
+   */
+  const postToTriggerDataLoad = useMemo(
+    () => Math.floor(postList.length * 0.75),
+    [postList]
+  );
+
+  /**
+   * autoplays the currently visible video and pauses others
+   */
+  useEffect(() => {
+    const postElements = document.querySelectorAll(".post-container");
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setVisiblePostId(entry.target.getAttribute("post-id"));
+          }
+        });
+      },
+      { threshold: 0.5 }
+    );
+
+    postElements.forEach((el) => observer.observe(el));
+    return () => postElements.forEach((el) => observer.unobserve(el));
+  }, [postToTriggerDataLoad]);
+
   return postList ? (
     postList?.map((post) => (
-      <Post
-        post={post}
-        canPlayVideo={true}
-        showDistance={false}
-        key={post.postId}
-      />
+      <div className="post-container" key={post.postId}>
+        <Post post={post} canPlayVideo={true} showDistance={false} />
+      </div>
     ))
   ) : (
     <EmptyFeed />
